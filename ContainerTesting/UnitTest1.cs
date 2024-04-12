6cdcc6f926413d755e7c3f95e89f25bea5f93760
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2024-04-12 19:09:34 +0300

    last test implemented

--- ContainerTesting/UnitTest1.cs
+++ ContainerTesting/UnitTest1.cs
@@ -26,6 +26,27 @@ namespace ContainerTesting
         }
     }
 
+    interface INew { }
+
+    class New : INew 
+    { 
+
+    }
+
+    class TestClass3 : ITest
+    {
+        public IRepo repo;
+        public IService<IRepo> service;
+        public INew test;
+
+        public TestClass3(IRepo repo, IService<IRepo> service, INew test)
+        {
+            this.repo = repo;
+            this.service = service;
+            this.test = test;
+        }
+    }
+
     interface IService<TRepo> where TRepo : IRepo { }
 
     class ServiceImpl<TRepo> : IService<TRepo> where TRepo : IRepo
@@ -104,6 +125,27 @@ namespace ContainerTesting
             Assert.NotNull(implementatiion);
             Assert.True(implementatiion is ServiceImpl<IRepo>);
             Assert.True((implementatiion as ServiceImpl<IRepo>).repo is Repo);
+        }
+
+        [Fact]
+        public void CheckResult_WithMultipleParams_InImplementation()
+        {
+            var config = new DependencyConfig();
+
+            config.Register<ITest, TestClass3>();
+            config.Register<IService<IRepo>, ServiceImpl<IRepo>>();
+            config.Register<IRepo, Repo>();
+            config.Register<INew, New>();
+
+            var provider = new DependencyProvider(config);
+
+            var implementatiion = provider.Resolve<ITest>();
+
+            Assert.NotNull(implementatiion);
+            Assert.True(implementatiion is TestClass3);
+            Assert.True((implementatiion as TestClass3).repo is Repo);
+            Assert.True((implementatiion as TestClass3).test is New);
+            Assert.True((implementatiion as TestClass3).service is ServiceImpl<IRepo>);
         }
     }
 }
